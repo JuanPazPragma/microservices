ec5a381f9d0decba8cb2d53ac99eb171fdd72ce8
JuanPazPragma <juan.pazg@pragma.com.co>
2023-02-21 12:21:31 -0500

    gradle

---
+++ plaza_comida_usuarios/src/main/java/com/example/usuarios/infrastructure/configuration/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.usuarios.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class WebSecurityConfig {
+
+}
--- plaza_comida_usuarios/src/main/java/com/example/usuarios/infrastructure/out/jpa/adapter/UserJpaAdapter.java
+++ plaza_comida_usuarios/src/main/java/com/example/usuarios/infrastructure/out/jpa/adapter/UserJpaAdapter.java
@@ -13,7 +13,6 @@ public class UserJpaAdapter implements IUserPersistencePort {
 
     private final IUserRepository userRepository;
     private final IUserEntityMapper userEntityMapper;
-
     @Autowired
     private PasswordEncoder passwordEncoder;
 
